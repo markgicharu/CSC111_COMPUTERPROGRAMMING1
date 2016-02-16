8400c0748f1bdd7bddd5711212b7f80c1814cbad
markgicharu <markgicharu@hotmail.com>
2016-02-16 18:40:55 +0300

    Update promptuser3integers.cpp

--- promptuser3integers.cpp
+++ promptuser3integers.cpp
@@ -5,6 +5,15 @@ using namespace std;
 //mainfunction begins here
 int main()
 {
+  //declare variables
+  int a,b,c;
+  //execution begins here
+  
+  //prompt user input
+  cout<<"please enter the first number"<<endl;
+  cin>>a;
+  cout<<"please enter the second number"<<endl;
+  cin>>b;
   
 }
 

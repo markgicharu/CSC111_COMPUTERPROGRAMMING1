ada70f426dfe98d88759bda9d2bb529e13f6d6d4
markgicharu <markgicharu@hotmail.com>
2016-02-16 18:10:20 +0300

    Update cat1q5.cpp

--- cat1q5.cpp
+++ cat1q5.cpp
@@ -1,1 +1,17 @@
-
+//preprocessor directive
+# include <iostream>
+using namespace std;
+//main function begins here
+int main()
+{
+  //declare variables
+  int a;
+  //execute
+  //prompt user input
+  cout<<"Enter an integer below"<<endl;
+  cin>>a;
+  
+  //runtime condition
+  
+  
+}

2fa61c88bf0e2824bd9b467a4f8f3f23d57c4db1
markgicharu <markgicharu@hotmail.com>
2016-02-16 19:07:14 +0300

    Update practicetest1.cpp

--- practicetest1.cpp
+++ practicetest1.cpp
@@ -5,9 +5,18 @@ using namespace std;
 int main()
 {
   //declare variables
-  double x,y,z;
+  double x,y,z,s,a,p;
   //prompt user input
-  cout<<"Please enter a number"
+  cout<<"Please enter a number"<<endl;
+  cin>>x;
+  cout<<"Please enter another number?"<<endl;
+  cin>>y;
+  cout<<"Please enter a final number?"<<endl;
+  cin>>z;
+  //compute
+  s=x+y+z;
+  a=s/3;
+  p=x*y*z;
   
   //terminate program
   return 0;

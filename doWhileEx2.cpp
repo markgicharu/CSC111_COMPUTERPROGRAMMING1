f97f16261e185c962c1772b7efe384dc509338e9
markgicharu <markgicharu@hotmail.com>
2016-02-23 19:51:21 +0300

    Create doWhileEx2.cpp

---
+++ doWhileEx2.cpp
@@ -0,0 +1,22 @@
+//This program executes the while loop for the hello world
+
+//preprocessor directive
+#include <iostream>
+using namespace std;
+
+//main functions begins here
+int main()
+{
+  //declare variables
+  int counter = 1;
+  
+  //execute while loop
+  while(counter<=10)
+  {
+    cout<<"Hello World!"<<endl;
+    counter ++;
+  }
+  //terminate the program
+  return 0;
+  
+}

58c7c6aef4810329405ed6bfd9018a3de6f58c1d
markgicharu <markgicharu@hotmail.com>
2016-02-23 19:43:16 +0300

    Update whileEx2.cpp

--- whileEx2.cpp
+++ whileEx2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+//main functions begins here
+int main()
+{
+  //declare variables
+  int counter = 1;
+  
+  //execute while loop
+  while(counter<=10)
+  {
+    cout<<"Hello World!"<<endl;
+  }
+  counter ++;
+  //terminate the program
+  return 0;
+  
+}

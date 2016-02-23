656944f8ddb89a17a16b028bc04078feb0abcadd
markgicharu <markgicharu@hotmail.com>
2016-02-23 20:12:09 +0300

    Create forLoopEx1.cpp

---
+++ forLoopEx1.cpp
@@ -0,0 +1,13 @@
+//preprocessor directive
+#include <iostream>
+using namespace std;
+
+//main functions begins here
+int main()
+{
+ for(int count=1; count<=10; count ++)
+ {
+  cout<<"Hello World!"<<endl;
+ }
+ return 0;
+}

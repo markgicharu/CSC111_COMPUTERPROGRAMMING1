e62a729adc40fe8b47fbc673dd6adef5d522af0f
markgicharu <markgicharu@hotmail.com>
2016-02-23 18:40:43 +0300

    Update switchstatement.cpp

--- switchstatement.cpp
+++ switchstatement.cpp
@@ -10,10 +10,10 @@ int main ()
 	//display the menu
 	cout<<"\t\t MENU"<<endl;
 	cout<<"code \t Item \t Price\n";
-	cout<<"code \t Hotdog \t 200 \n";
-	cout<<"code \t Pizza \t 700 \n";
-	cout<<"code \t Chips \t 150 \n";
-	cout<<"code \t Chicken \t 100 \n";
+	cout<<"1 \t \t Hotdog \t 200 \n";
+	cout<<"2 \t \t Pizza \t 700 \n";
+	cout<<"3 \t \t Chips \t 150 \n";
+	cout<<"4 \t \t Chicken \t 100 \n";
 	
 	//prompt user for input
 	cout<<"Kindly enter a menu code"<<endl;
@@ -27,15 +27,15 @@ int main ()
 			break;
 			
 			case 2:
-			cout<<"You have selected Hotdog costing Kshs. 200";
+			cout<<"You have selected pizza costing Kshs. 700";
 			break;
 			
 			case 3:
-			cout<<"You have selected Hotdog costing Kshs. 200";
+			cout<<"You have selected Chips costing Kshs. 150";
 			break;
 			
 			case 4:
-			cout<<"You have selected Hotdog costing Kshs. 200";
+			cout<<"You have selected Chicken costing Kshs. 100";
 			break;
 
 }

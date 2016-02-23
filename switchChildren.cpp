56dd39cc9a723c0f701a0bb9d224e44d70c1b6a9
markgicharu <markgicharu@hotmail.com>
2016-02-23 19:04:44 +0300

    Update switchChildren.cpp

--- switchChildren.cpp
+++ switchChildren.cpp
@@ -14,4 +14,12 @@ int main()
   cout<<"Please enter the gender/sex of the child"<<endl;
   cin>>gender;
   
+  //generate switch statement
+  switch(age)
+  {
+    case age:
+    cout<<"The age of the child is "<<age<<"the sex of the child is "<<gender<<endl;
+    break;
+    
+  }
 }

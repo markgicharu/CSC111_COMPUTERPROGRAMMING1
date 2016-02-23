87c32ac41b6baf3a00426367806fa34d7883ef63
markgicharu <markgicharu@hotmail.com>
2016-02-23 18:35:08 +0300

    Update switchstatement.cpp

--- switchstatement.cpp
+++ switchstatement.cpp
@@ -10,5 +10,9 @@ int main ()
 	//display the menu
 	cout<<"\t\t MENU"<<endl;
 	cout<<"code \t Item \t Price\n";
+	cout<<"code \t Hotdog \t 200 \n";
+	cout<<"code \t Pizza \t 700 \n";
+	cout<<"code \t Chips \t 150 \n";
+	cout<<"code \t Chicken \t 100 \n";
 
 }

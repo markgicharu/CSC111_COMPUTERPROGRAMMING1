6defe45e3623d0a9e6edf71bd00f1951b6cb83d8
markgicharu <markgicharu@hotmail.com>
2016-03-01 18:40:08 +0300

    Update arrayexample1.cpp

--- arrayexample1.cpp
+++ arrayexample1.cpp
@@ -1,11 +1,16 @@
+//this ia a simple example of an array
+//preprocessor directive
 #include <iostream>
 using namespace std;
-
+//mainfunction starts here
 int main()
 {
+	//declare and initialize the array
 	int box[4]={70,80,40,10};
+	
 	//display the value of 70
 	cout<<box[3]<<endl;
+	
 	//terminate the program
 	return 0;
 }

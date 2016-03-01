1205b022207410ab505b4887b603df0f59613172
markgicharu <markgicharu@hotmail.com>
2016-03-01 19:32:55 +0300

    Update array2ClassPractice.cpp

--- array2ClassPractice.cpp
+++ array2ClassPractice.cpp
@@ -8,8 +8,17 @@ int main ()
     int arr[10];
     
     //for loop begins here
+    for(int i=0; i<0; i++)
+    {
+      cin>> arr[i];
+      cout<<"Values in array are now: ";
+    }
+    //Accessing the 10 integers
+    for (int i=0; i<10; i++ )
+    {
+      cout<< arr[i]<<", ";
+    }
     
-    for(int x=0; intx>0; x++)
     
     
   }

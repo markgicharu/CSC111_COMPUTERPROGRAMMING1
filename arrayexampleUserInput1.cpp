29fe6d58431f5f01cc45738043b473e710218da4
markgicharu <markgicharu@hotmail.com>
2016-03-01 19:19:10 +0300

    Create arrayexampleUserInput1.cpp

---
+++ arrayexampleUserInput1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//declare variable
+	int mark[5];
+
+	for(int x=5; x>0; x--)
+	//display the value of 70
+	cout<<"Mark"<<x+1<<"=";
+	cin>>mark[0];
+	cin>>mark[1];
+	cin>>mark[2];
+	cin>>mark[3];
+	cin>>mark[4];
+
+	//terminate the program
+	return 0;
+}

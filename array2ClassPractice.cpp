09380c723a35f41bc5e9aa7d73acf6a3fb63480b
markgicharu <markgicharu@hotmail.com>
2016-03-01 19:35:49 +0300

    Update array2ClassPractice.cpp

--- array2ClassPractice.cpp
+++ array2ClassPractice.cpp
@@ -6,19 +6,22 @@ int main ()
   {
     //declare array variables
     int arr[10];
+    cout<< "Please enter 10 integers: "<<endl;
     
-    //for loop begins here
+    //for loop begins here for storing integers
     for(int i=0; i<0; i++)
     {
+      cout<<"Element "<<i+1<<"=";
       cin>> arr[i];
-      cout<<"Values in array are now: ";
+     
     }
     //Accessing the 10 integers
+    cout<<"Values in array are now: ";
     for (int i=0; i<10; i++ )
     {
       cout<< arr[i]<<", ";
     }
     
-    
-    
+    return 0;
+   
   }

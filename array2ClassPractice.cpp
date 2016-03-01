695ab5f58ca97711dd42d9638740291be9f235ab
markgicharu <markgicharu@hotmail.com>
2016-03-01 19:12:47 +0300

    Update array2ClassPractice.cpp

--- array2ClassPractice.cpp
+++ array2ClassPractice.cpp
@@ -4,6 +4,12 @@ using namespace std;
 //mainfunction begins here
 int main ()
   {
+    //declare array variables
+    int arr[10];
+    
+    //for loop begins here
+    
+    for(int x=0; intx>0; x++)
     
     
   }

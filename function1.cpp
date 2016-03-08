0025bfca10e2c6abf8b40c5e7ca288047129cddc
markgicharu <markgicharu@hotmail.com>
2016-03-08 19:55:19 +0300

    Update function1.cpp

--- function1.cpp
+++ function1.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
   {
+    int m, n;
+    cout<<"Kindly enter the value of m:";
+    cin>>m;
+    cout<<"Kindly enter the value of n:";
+    cin>>n;
+    cout<<"addition(m,n)";
     
-  };
+    return 0;
+  }

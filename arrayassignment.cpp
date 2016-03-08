38bcac27672f93e4b11162312eb9f138cc40aba7
markgicharu <markgicharu@hotmail.com>
2016-03-08 16:39:06 +0300

    Update arrayassignment.cpp

--- arrayassignment.cpp
+++ arrayassignment.cpp
@@ -4,6 +4,15 @@ using namespace std;
 //main function begins here
 int main()
 {
+    //declare array variable
+    int arr[10];
+    //input output to user
+    cout<<"Please enter an integer?"
     
+    //conditional statement
+    for(int x; x==""; x)
+    {
+        cout<<"please enter an integer?"
+    }
   
 };

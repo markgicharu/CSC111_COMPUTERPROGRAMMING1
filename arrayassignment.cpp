7e6243a5541b8af6522de238ab49d641828e0e7b
markgicharu <markgicharu@hotmail.com>
2016-03-08 16:49:05 +0300

    Update arrayassignment.cpp

--- arrayassignment.cpp
+++ arrayassignment.cpp
@@ -7,12 +7,18 @@ int main()
     //declare array variable
     int arr[10];
     //input output to user
-    cout<<"Please enter an integer?"
+    cout<<"Please enter an integer?";
     
     //conditional statement
-    for(int x; x==""; x)
+    for(int x=arr; x=""; x)
     {
-        cout<<"please enter an integer?"
+        cout<<"please enter an integer?";
+        cin>>arr[];
     }
+  //display the output statement
+  cout<<"the values you entered are:\n";
+  cout<<arr[]<<endl;
   
+  //terminate the program
+  return 0;
 };

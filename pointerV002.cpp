3accf7be1e7c66117c1d1fa35eb17d2389618f7e
markgicharu <markgicharu@hotmail.com>
2016-03-08 18:51:14 +0300

    Create pointerV002.cpp

---
+++ pointerV002.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+  {
+    //declaring variables
+    int x = 10;
+    int y = 200;
+    
+    //declaring pi=ointers
+    
+    int myPointer;
+    
+    //initializing  a pointer
+    
+  };

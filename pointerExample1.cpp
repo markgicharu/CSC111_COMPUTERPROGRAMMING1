7c2e4c5cc0446f3ca09d34e3fd4136f52d3185cb
markgicharu <markgicharu@hotmail.com>
2016-03-08 19:10:38 +0300

    Create pointerExample1.cpp

---
+++ pointerExample1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+  {
+    //declaring variables
+    int x = 10;
+    int y = 200;
+    
+    //declaring pointers
+    
+    int *myPointer;
+    
+    //initializing  a pointer
+    myPointer = &y; // pointer points to variable y
+    
+    //print values printed by myPointer
+    cout<<"myPointer points to a value: ";
+    cout<<*myPointer<<endl;
+    
+    //print the address stored by myPointer
+    cout<<"The address is "<<myPointer;
+    
+    //using dereference pointer as a value
+    *myPointer = 600; //reassinging value pointed to bymyPointer
+    
+    cout<<"The value of y = "<<y;
+    cout<<"\n or"<<*myPointer<<endl;
+    
+    //display the address of variable x and y
+    cout<<"the address of x is "<<&y;
+    cout<<"\n address of y is "<<myPointer<<endl;
+    
+  };

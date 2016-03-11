16d65fc295a2357376c337d7c38a7fa9755a6c04
markgicharu <markgicharu@hotmail.com>
2016-03-11 17:49:25 +0300

    Create switchCaseChoice.cpp

---
+++ switchCaseChoice.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int choice;
+	 cout<<"1.dog"<<endl;
+	cout<<"2.elephant"<<endl;
+	cout<<"3.lion"<<endl;
+	cout<<"Enter the animal you want: "<<endl;
+	switch=choice
+	{
+	cin<<choice<<endl;
+	{
+	case 1:
+		cout<<"10,000"<<endl;
+		break;
+   case 2:
+		cout<<"20,000"<<endl;
+		break;
+   case 3:
+		cout<<"30,000"<<endl;
+		break;
+default:
+		cout<<"30,000"<<endl;
+		break;
+	}
+	}
+
+
